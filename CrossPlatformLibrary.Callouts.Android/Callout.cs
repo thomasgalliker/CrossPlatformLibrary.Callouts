502180023af031d75b2ea57063a23b05a8fd0ab1
thomasgalliker <thomas@bluewin.ch>
2016-01-14 20:26:03 +0100

    Add Callout for iOS (throwing NotImplementedException)

--- CrossPlatformLibrary.Callouts.Android/Callout.cs
+++ CrossPlatformLibrary.Callouts.Android/Callout.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
+using Android;
 using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
 
 using CrossPlatformLibrary.Dispatching;
 
 using Guards;
+
+using Java.Lang;
+using Java.Lang.Reflect;
+using Java.Util;
+
+using Exception = System.Exception;
+using Object = System.Object;
 
 namespace CrossPlatformLibrary.Callouts
 {
@@ -27,10 +40,10 @@ namespace CrossPlatformLibrary.Callouts
         {
             Guard.ArgumentNotNull(() => buttonConfigs);
 
-            var context = Application.Context;
-
-            var alert = new AlertDialog.Builder(context);
+            var currentActivity = GetActivity();
+            var alert = new AlertDialog.Builder(currentActivity);
             alert.SetTitle(caption);
+            alert.SetCancelable(false);
 
             var stringContent = content as string;
             if (stringContent != null)
@@ -38,14 +51,54 @@ namespace CrossPlatformLibrary.Callouts
                 alert.SetMessage(stringContent);
             }
 
+            var viewContent = content as View;
+            if (viewContent != null)
+            {
+                alert.SetView(viewContent);
+            }
+
             foreach (var buttonConfig in buttonConfigs)
             {
                 var config = buttonConfig;
-                alert.SetNeutralButton(buttonConfig.Text, (senderAlert, args) => { config.Action(); });
+                alert.SetPositiveButton(buttonConfig.Text, (senderAlert, args) => { config.Action(); });
+                ////alert.SetNegativeButton(buttonConfig.Text, (senderAlert, args) => { config.Action(); });
+                ////alert.SetNeutralButton(buttonConfig.Text, (senderAlert, args) => { config.Action(); });
             }
 
             // dispatch the alert to the UI thread
-            this.dispatcherService.CheckBeginInvokeOnUI(() => alert.Show());
+            this.dispatcherService.CheckBeginInvokeOnUI(() => alert.Create().Show());
+        }
+
+        private static Activity GetActivity()
+        {
+            Class activityThreadClass = Class.ForName("android.app.ActivityThread");
+            Java.Lang.Object activityThread = activityThreadClass.GetMethod("currentActivityThread").Invoke(null);
+            var activitiesField = activityThreadClass.GetDeclaredField("mActivities");
+            activitiesField.Accessible = true;
+            Android.Util.ArrayMap activities = (Android.Util.ArrayMap)activitiesField.Get(activityThread);
+            foreach (Java.Lang.Object activityRecord in activities.Values())
+            {
+                try
+                {
+                    Class activityRecordClass = activityRecord.Class;
+                    Field pausedField = activityRecordClass.GetDeclaredField("paused");
+                    pausedField.Accessible = true;
+                    if (!pausedField.GetBoolean(activityRecord))
+                    {
+                        Field activityField = activityRecordClass.GetDeclaredField("activity");
+                        activityField.Accessible = true;
+                        Activity activity = (Activity)activityField.Get(activityRecord);
+                        return activity;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+            return null;
         }
     }
 }
---
+++ CrossPlatformLibrary.Callouts.iOS/Callout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using CrossPlatformLibrary.Dispatching;
+
+using Guards;
+
+namespace CrossPlatformLibrary.Callouts
+{
+    /// <summary>
+    /// Callout implementation for iOS.
+    /// </summary>
+    public class Callout : CalloutBase
+    {
+        private readonly IDispatcherService dispatcherService;
+
+        public Callout(IDispatcherService dispatcherService)
+        {
+            Guard.ArgumentNotNull(() => dispatcherService);
+
+            this.dispatcherService = dispatcherService;
+        }
+
+        /// <inheritdoc />
+        public override void Show(string caption, object content, ButtonConfig[] buttonConfigs, bool isFullScreen = false)
+        {
+            Guard.ArgumentNotNull(() => buttonConfigs);
+
+            throw new NotImplementedException();
+        }
+    }
+}
