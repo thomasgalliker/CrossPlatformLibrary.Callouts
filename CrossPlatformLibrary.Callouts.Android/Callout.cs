a5f92dc4771a817c4bc9f54f53b79c7f644a0e1b
thomasgalliker <thomas@bluewin.ch>
2016-01-14 18:44:56 +0100

    Add Callout implementation for Android (initial commit)

---
+++ CrossPlatformLibrary.Callouts.Android/Callout.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Android.App;
+
+using CrossPlatformLibrary.Dispatching;
+
+using Guards;
+
+namespace CrossPlatformLibrary.Callouts
+{
+    /// <summary>
+    /// Callout implementation for Android.
+    /// </summary>
+    public class Callout : CalloutBase
+    {
+        private readonly IDispatcherService dispatcherService;
+
+        public Callout(IDispatcherService dispatcherService)
+        {
+            Guard.ArgumentNotNull(() => dispatcherService);
+
+            this.dispatcherService = dispatcherService;
+        }
+
+        /// <inheritdoc />
+        public override void Show(string caption, object content, ButtonConfig[] buttonConfigs, bool isFullScreen = false)
+        {
+            Guard.ArgumentNotNull(() => buttonConfigs);
+
+            var context = Application.Context;
+
+            var alert = new AlertDialog.Builder(context);
+            alert.SetTitle(caption);
+
+            var stringContent = content as string;
+            if (stringContent != null)
+            {
+                alert.SetMessage(stringContent);
+            }
+
+            foreach (var buttonConfig in buttonConfigs)
+            {
+                var config = buttonConfig;
+                alert.SetNeutralButton(buttonConfig.Text, (senderAlert, args) => { config.Action(); });
+            }
+
+            // dispatch the alert to the UI thread
+            this.dispatcherService.CheckBeginInvokeOnUI(() => alert.Show());
+        }
+    }
+}
