b470bc37fc982ea47836354dfb974a890f81146d
thomasgalliker <thomas@bluewin.ch>
2016-01-14 20:28:07 +0100

    Add Xamarin Forms sample app

---
+++ Samples/CalloutsSample/CalloutsSample/DemoPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using CrossPlatformLibrary.Callouts;
+using CrossPlatformLibrary.IoC;
+
+using Xamarin.Forms;
+
+namespace CalloutsSample
+{
+    class DemoPage : ContentPage
+    {
+        public DemoPage()
+        {
+            this.Title = "CrossPlatformLibrary.Callouts Demo";
+
+            var button = new Button
+            {
+                Text = "Show simple callout"
+            };
+            button.Clicked += (sender, args) =>
+            {
+                var callout = SimpleIoc.Default.GetInstance<ICallout>();
+
+                var buttonConfigs = new[] { new ButtonConfig("I Agree", () => { }), new ButtonConfig("I Decline") };
+
+                callout.Show("Simple Callout", "Message with some text.", buttonConfigs);
+            };
+            
+            var stackPanel = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { button }
+            };
+
+            this.Content = stackPanel;
+        }
+    }
+}
