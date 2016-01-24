d2ba364670ad6d0899050e65ff8caa5534312b9a
thomasgalliker <thomas@bluewin.ch>
2016-01-24 21:22:23 +0100

    Add support for WPF (work-in-progress)

---
+++ CrossPlatformLibrary.Callouts.Netfx/Callout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+using CrossPlatformLibrary.Dispatching;
+
+using Guards;
+
+using WPFCustomMessageBox;
+
+namespace CrossPlatformLibrary.Callouts
+{
+    /// <summary>
+    /// Callout implementation for .Net/WPF.
+    /// </summary>
+    public class Callout : CalloutBase
+    {
+        /// <inheritdoc />
+        public override void Show(string caption, object content, ButtonConfig[] buttonConfigs, bool isFullScreen = false)
+        {
+            Guard.ArgumentNotNull(() => buttonConfigs);
+
+            var result = CustomMessageBox.Show(
+                messageBoxText: (string)content, 
+                caption: caption, button: MessageBoxButton.OK);
+        }
+    }
+}
