b1882a92767b9c17a654dc93ed0c3cd4d0de95b8
thomasgalliker <thomas@bluewin.ch>
2016-01-25 22:29:04 +0100

    Add string callout support for iOS

--- CrossPlatformLibrary.Callouts.iOS/Callout.cs
+++ CrossPlatformLibrary.Callouts.iOS/Callout.cs
@@ -4,6 +4,12 @@ using CrossPlatformLibrary.Dispatching;
 
 using Guards;
 
+#if __UNIFIED__
+using UIKit;
+#else
+using MonoTouch.UIKit;
+#endif
+
 namespace CrossPlatformLibrary.Callouts
 {
     /// <summary>
@@ -25,7 +31,57 @@ namespace CrossPlatformLibrary.Callouts
         {
             Guard.ArgumentNotNull(() => buttonConfigs);
 
-            throw new NotImplementedException();
+            var stringContent = content as string;
+            if (stringContent == null)
+            {
+                throw new NotImplementedException("Content dialogs are not yet supported.");
+            }
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var alertController = UIAlertController.Create(caption, string.Empty, UIAlertControllerStyle.Alert);
+                if (stringContent != null)
+                {
+                    alertController.Message = stringContent;
+                }
+                else
+                {
+                    // TODO Implement content dialogs
+                }
+
+                foreach (var buttonConfig in buttonConfigs)
+                {
+                    alertController.AddAction(UIAlertAction.Create(buttonConfig.Text, UIAlertActionStyle.Default, x => buttonConfig.Action()));
+                }
+
+                this.dispatcherService.CheckBeginInvokeOnUI(() => { UIApplication.SharedApplication.PresentInternal(alertController); });
+            }
+            else
+            {
+                var alertView = new UIAlertView
+                {
+                    AlertViewStyle = UIAlertViewStyle.LoginAndPasswordInput,
+                    Title = caption
+                };
+
+                if (stringContent != null)
+                {
+                    alertView.Message = stringContent;
+                }
+                else
+                {
+                    // TODO Implement content dialogs
+                }
+
+                foreach (var buttonConfig in buttonConfigs)
+                {
+                    alertView.AddButton(buttonConfig.Text);
+                }
+
+                alertView.Clicked += (s, e) => { buttonConfigs[e.ButtonIndex].Action(); };
+
+                this.dispatcherService.CheckBeginInvokeOnUI(() => { alertView.Show(); });
+            }
         }
     }
 }
---
+++ CrossPlatformLibrary.Callouts.iOS/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+#if __UNIFIED__
+using CoreGraphics;
+using UIKit;
+#else
+using MonoTouch.UIKit;
+#endif
+
+namespace CrossPlatformLibrary.Callouts
+{
+    public static class Extensions
+    {
+        internal static void PresentInternal(this UIApplication app, UIViewController controller)
+        {
+            var topViewController = GetTopViewController(app);
+            if (controller.PopoverPresentationController != null)
+            {
+#if __UNIFIED__
+                var cgRect = new CGRect(topViewController.View.Bounds.Width / (nfloat)2, topViewController.View.Bounds.Bottom, (nfloat)0, (nfloat)0);
+#else
+                var cgRect = new RectangleF(topViewController.View.Bounds.Width / 2, topViewController.View.Bounds.Bottom, 0, 0);
+#endif
+                controller.PopoverPresentationController.SourceView = topViewController.View;
+                controller.PopoverPresentationController.SourceRect = cgRect;
+                controller.PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
+            }
+
+            topViewController.PresentViewController(controller, animated: false, completionHandler: () => { });
+        }
+
+
+        private static UIViewController GetTopViewController(this UIApplication app)
+        {
+            UIViewController uiViewController = app.KeyWindow.RootViewController;
+            while (uiViewController.PresentedViewController != null)
+            {
+                uiViewController = uiViewController.PresentedViewController;
+            }
+            return uiViewController;
+        }
+
+    }
+}
