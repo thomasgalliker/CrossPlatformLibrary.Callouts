d9573fd8402728cf39ec6b0450f210c0a9c824f8
thomasgalliker <thomas@bluewin.ch>
2016-04-06 16:26:41 +0200

    Add CustomCallout content for iOS Callouts

---
+++ Samples/CalloutsSample/CalloutsSample.iOS/CustomCallout.cs
@@ -0,0 +1,20 @@
+﻿using CoreGraphics;
+
+using CrossPlatformLibrary.Callouts;
+
+using UIKit;
+
+namespace CalloutsSample.Forms.iOS
+{
+    public class CustomCallout : ICustomCallout
+    {
+        public object GetContent(ButtonConfig okButtonConfig)
+        {
+            var label = new UILabel();
+            label.Text = "A custom iOS UILabel with some text...";
+            label.SizeToFit();
+            label.AdjustsFontSizeToFitWidth = true;
+            return label;
+        }
+    }
+}
--- Samples/CalloutsSample/CalloutsSample.iOS/Main.cs
+++ Samples/CalloutsSample/CalloutsSample.iOS/Main.cs
@@ -1,4 +1,6 @@
-﻿using UIKit;
+﻿using CrossPlatformLibrary.IoC;
+
+using UIKit;
 
 namespace CalloutsSample.Forms.iOS
 {
@@ -7,6 +9,8 @@ namespace CalloutsSample.Forms.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            SimpleIoc.Default.Register<ICustomCallout, CustomCallout>(); // TODO: Register by convention!
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
