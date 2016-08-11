d27c06dbb63b710ae95fc5f6ab6953fc3bca724b
thomasgalliker <thomas@bluewin.ch>
2016-08-11 16:52:24 +0200

    Update nuget dependencies

--- Samples/CalloutsSample/CalloutsSample.iOS/LinkerHelper.cs
+++ Samples/CalloutsSample/CalloutsSample.iOS/LinkerHelper.cs
@@ -3,4 +3,3 @@
 // This assembly annotation is essential for CrossPlatformLibrary to work correctly in Xamarin.iOS and MonoTouch projects.
 // It prevents the Xamarin linker from removing 'unused' code.
 [assembly: Preserve(typeof(CrossPlatformLibrary.LinkerHook), AllMembers = true)]
-[assembly: Preserve(typeof(CrossPlatformLibrary.Callouts.Callout), AllMembers = true)]
